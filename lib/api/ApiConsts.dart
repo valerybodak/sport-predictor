e83001893ddad7e54bdde1dc0d636647fe194582
Valeriy Bodak <valery.bodak@exposit.com>
2019-03-28 11:20:02 +0300

    .gitignore is now working

--- lib/api/ApiConsts.dart
+++ lib/api/ApiConsts.dart
@@ -1,5 +1,5 @@
 class ApiConsts {
 
   static const API_FOOTBALL_ENDPOINT = "https://apifootball.com/api/";
-  static const API_FOOTBALL_KEY = "setup-your-key-here";
+  static const API_FOOTBALL_KEY = "3586962b18c3c436b237a791561e737dd1e98e56f9a5ac6e4b7bc61f3df57323";
 }
